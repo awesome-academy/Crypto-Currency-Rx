421ffd900a774b89068760b880c589a4eb93e741
namtrinh <sabo_nt>
2021-08-23 15:26:12 +0700

    Refs[TASK][40714] Init Project

---
+++ Crypto-Currency-Rx/Crypto-Currency-Rx/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Crypto-Currency-Rx
+//
+//  Created by namtrinh on 23/08/2021.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
